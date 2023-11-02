2a9c0cc4a37814eb083891b39b88fefaca580128
yun0205yun <q0926250912@gmail.com>
2023-11-02 17:58:11 +0800

    要加mvcpaging ajax

--- Models/MessageViewModel.cs
+++ Models/MessageViewModel.cs
@@ -1,9 +1,8 @@
 ﻿using myhw.Models;
-using PagedList;
 using System.Web.Mvc;
 
 internal class MessageViewModel  
 {
     public string Username { get; set; }
-    public IPagedList<MessageDataModel> Messages { get; set; }
+   
 }
