0db267d15e881a0842139e94e29f91eca6a19e45
yun0205yun <147116492+yun0205yun@users.noreply.github.com>
2023-11-08 15:48:37 +0800

    Delete dbschema/Users

--- dbschema/MessageContent.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Content (
-    ContentId INT PRIMARY KEY IDENTITY(1,1),
-    UserId INT FOREIGN KEY REFERENCES Users(UserId),
-    Content NVARCHAR(1000) NOT NULL,
-    CreatedAt DATETIME DEFAULT GETDATE() -- 將 CreatedAt 添加到 Content 表中，默認值為當前日期和時間
-);
--- dbschema/error log.sql
+++
@@ -1,8 +0,0 @@
-create table ErrorLog
-(
-	ErrorId INT PRIMARY KEY IDENTITY(1,1),
-    ErrorMessage NVARCHAR(MAX),
-    LogTime DATETIME
-
-¡@¡@
-);
